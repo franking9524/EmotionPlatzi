d91d45c7d434aa77dfa167cd278016e96ed02493
Frank Vargas <frank_9524@hotmail.es>
2020-06-19 15:58:51 -0500

    testing purposes

---
+++ EmotionPlatzi.Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using EmotionPlatzi.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmotionPlatzi.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.WelcomeMessage = "Hola Mundo";
+            ViewBag.Autor = "ING. FRANK ALEXANDER MONSALVE VARGAS";
+            return View();
+        }
+        public ActionResult IndexAlt()
+        {
+            var modelo = new Home();
+            modelo.WelcomeMessage="Hola Mundo desde el modelo";
+            return View(modelo);
+        }
+    }
+}
---
+++ EmotionPlatzi.Web/Models/EmoPicture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace EmotionPlatzi.Web.Models
+{
+    public class EmoPicture
+    {
+        public int Id { get; set; }
+        [Display(Name ="Nombre")]
+        public string Nombre { get; set; }
+        //[Required]
+        //[MaxLength(1000, ErrorMessage = "La ruta supera el rango establecido")]
+        [Display(Name = "Ruta")]
+        public string Path { get; set; }
+
+        public virtual ObservableCollection<EmoFace> Faces { get; set; }
+    }
+}
